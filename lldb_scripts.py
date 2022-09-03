da21c0c37e0f048fda51287c6342b774093e7e27
Carl Nysten <nysten.carl@gmail.com>
2022-09-03 22:54:49 +0300

    lldb formatter for vm added

---
+++ lldb_scripts.py
@@ -0,0 +1,28 @@
+import lldb
+
+def __lldb_init_module(debugger, internal_dict):
+    debugger.HandleCommand("type summary add -F lldb_scripts.vm_summary t_vm")
+    pass
+
+reg_names = [
+    'R_R0',
+    'R_R1',
+    'R_R2',
+    'R_R3',
+    'R_R4',
+    'R_R5',
+    'R_R6',
+    'R_R7',
+    'R_PC',
+    'R_COND',
+    'R_COUNT'
+]
+def vm_summary(valobj, internal_dict, options):
+    summary_string = "{\n"
+    memory, regs = valobj.get_value_child_list()
+    summary_string += "  " + str(memory) + "\n"
+    summary_string += "  (" + regs.GetTypeName() + ") regs = {\n"
+    for i, reg in enumerate(regs):
+        summary_string += "    {}: {},\n".format(reg_names[i], reg.GetValue())
+    summary_string += "  }\n}"
+    return summary_string
