6285c952e60ad076f8b5efa624ae7c14ef68ec9f
cnysten <cnysten@student.hive.fi>
2022-09-05 11:08:04 +0300

    trap_puts implemented

--- src/trap.c
+++ src/trap.c
@@ -6,11 +6,27 @@
 /*   By: carlnysten <marvin@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/04 16:29:17 by carlnysten        #+#    #+#             */
-/*   Updated: 2022/09/04 16:29:26 by carlnysten       ###   ########.fr       */
+/*   Updated: 2022/09/05 11:07:25 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lc3vm.h"
+#include <stdio.h>
+
+/* Writes a null-terminated string of ASCII characters to the STDOUT.
+ * Each character is stored not as a byte, but a uint16_t */
+void	trap_puts(t_vm *vm)
+{
+	uint16_t	*c;
+
+	c = vm->memory + vm->regs[R_R0];
+	while (*c)
+	{
+		putc((char) *c, stdout);
+		c++;
+	}
+	fflush(stdout);
+}
 
 /* Encoding
  *
@@ -23,6 +39,16 @@ void	trap(uint16_t instr, t_vm *vm)
 	uint16_t	trapcode;
 
 	trapcode = instr & 0xff;
-	if (trapcode == 0x25)
+	if (trapcode == TRAP_GETC)
+		return ;
+	else if (trapcode == TRAP_OUT)
+		return ;
+	else if (trapcode == TRAP_PUTS)
+		trap_puts(vm);
+	else if (trapcode == TRAP_IN)
+		return ;
+	else if (trapcode == TRAP_PUTSP)
+		return ;
+	else if (trapcode == TRAP_HALT)
 		vm->running = 0;
 }
