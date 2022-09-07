4e50ed3a90b2dea220415b8c13f685f5c821e460
cnysten <cnysten@student.hive.fi>
2022-09-07 15:11:45 +0300

    lexer returns array of strings

--- assembler/src/main.c
+++ assembler/src/main.c
@@ -6,18 +6,19 @@
 /*   By: cnysten <cnysten@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 11:28:45 by cnysten           #+#    #+#             */
-/*   Updated: 2022/09/07 13:58:29 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/09/07 15:10:52 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
+
+#define SEPARATORS " \n\t"
 
 char	*read_file(char *filename)
 {
 	FILE	*file;
 	size_t	filesize;
 	char	*contents;
-	/* char	buffer[512]; */
 
 	file = fopen(filename, "r");
 	if (!file)
@@ -25,7 +26,7 @@ char	*read_file(char *filename)
 	fseek(file, 0L, SEEK_END);
 	filesize = ftell(file);
 	fseek(file, 0L, SEEK_SET);
-	printf("Filesize: %zu\n", filesize);
+	/* printf("Filesize: %zu\n", filesize); */
 	contents = malloc(filesize);
 	if (!contents)
 		return (NULL);
@@ -33,15 +34,56 @@ char	*read_file(char *filename)
 	return (contents);
 }
 
+char	**lex(char *contents)
+{
+	char	**lexemes;
+	char	*str;
+	size_t	size;
+	size_t	len;
+
+	size = 1;
+	len = 0;
+	lexemes = malloc(size * sizeof (char *));
+	str = strtok(contents, SEPARATORS);
+	while (str != NULL)
+	{
+		/* printf("'%s', ", str); */
+		if (len == size)
+		{
+			lexemes = realloc(lexemes, size * 2 * sizeof (char *));
+			size *= 2;
+		}
+		lexemes[len++] = str;
+		str = strtok(NULL, SEPARATORS);
+	}
+	for (size_t i = 0; i < len; i++)
+		printf("'%s', ", lexemes[i]);
+	return (lexemes);
+}
+
+t_token	*tokenize(char **lexemes)
+{
+	(void) lexemes;
+	return (NULL);
+}
+
+uint16_t	*map_to_instructions(t_token *tokens)
+{
+	(void) tokens;
+	return (NULL);
+}
+
 int	main(int argc, char **argv)
 {
-	char	*contents;
+	char		*contents;
+	uint16_t	*output;
 
 	if (argc < 2)
 		return (puts("Usage: little-assembler [asm-file]\n"), 0);
 	contents = read_file(argv[1]);
 	if (!contents)
 		return (-1);
-	printf("%s", contents);
+	/* printf("%s", contents); */
+	output = map_to_instructions(tokenize(lex(contents)));
 	return (0);
 }
