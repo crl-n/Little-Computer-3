6011c28db81a8d89ce652549fd311ccf0ccb6be5
cnysten <cnysten@student.hive.fi>
2022-09-07 14:00:01 +0300

    read_file implemented

--- assembler/includes/assembler.h
+++ assembler/includes/assembler.h
@@ -6,12 +6,16 @@
 /*   By: cnysten <cnysten@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 11:16:33 by cnysten           #+#    #+#             */
-/*   Updated: 2022/09/07 11:28:28 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/09/07 13:55:38 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef ASSEMBLER_H
 # define ASSEMBLER_H
+
+# include <stdio.h>
+# include <string.h>
+# include <stdlib.h>
 
 enum e_type
 {
--- assembler/src/main.c
+++ assembler/src/main.c
@@ -6,16 +6,42 @@
 /*   By: cnysten <cnysten@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 11:28:45 by cnysten           #+#    #+#             */
-/*   Updated: 2022/09/07 11:31:04 by cnysten          ###   ########.fr       */
+/*   Updated: 2022/09/07 13:58:29 by cnysten          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "assembler.h"
 
-int	main(void)
+char	*read_file(char *filename)
 {
-	t_token	token;
+	FILE	*file;
+	size_t	filesize;
+	char	*contents;
+	/* char	buffer[512]; */
 
-	token = (t_token){OPERAND, "R1"};
+	file = fopen(filename, "r");
+	if (!file)
+		return (puts("Error: could not open file\n"), NULL);
+	fseek(file, 0L, SEEK_END);
+	filesize = ftell(file);
+	fseek(file, 0L, SEEK_SET);
+	printf("Filesize: %zu\n", filesize);
+	contents = malloc(filesize);
+	if (!contents)
+		return (NULL);
+	fread(contents, sizeof (char), filesize, file);
+	return (contents);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*contents;
+
+	if (argc < 2)
+		return (puts("Usage: little-assembler [asm-file]\n"), 0);
+	contents = read_file(argv[1]);
+	if (!contents)
+		return (-1);
+	printf("%s", contents);
 	return (0);
 }
